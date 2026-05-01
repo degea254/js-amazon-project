5f4b937667ad2a4b54af08e6a16c6317e3bef6df
degea <josekathata@gmail.com>
2026-05-01 12:30:05 +0300

    make delivery option interactive

--- data/cart.js
+++ data/cart.js
@@ -5,13 +5,13 @@ if (!cart) {
             {
                 productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
                 quantity: 2,
-                deliveryOptionId:'1'
+                deliveryOptionId: '1'
             },
 
             {
                 productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
                 quantity: 3,
-                deliveryOptionId:'2'
+                deliveryOptionId: '2'
             }
         ]
 }
@@ -39,7 +39,7 @@ export function addToCart(productId) {
         cart.push({
             productId: productId,
             quantity: 1,
-            deliveryOptionId:'1'
+            deliveryOptionId: '1'
         })
     }
 
@@ -59,3 +59,17 @@ export function removeFromCart(productId) {
 
     saveToStorage()
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingitem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingitem = cartItem
+        }
+    })
+
+    matchingitem.deliveryOptionId = deliveryOptionId;
+
+    saveToStorage()
+} 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
@@ -41,8 +41,8 @@ cart.forEach((cartItem) => {
   })
 
   const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days')
-    const dateString = deliveryDate.format('dddd, MMMM, D')
+  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days')
+  const dateString = deliveryDate.format('dddd, MMMM, D')
 
 
 
@@ -104,7 +104,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
 
     html +=
       `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+              data-product-id="${matchingProduct.id}"
+              data-delivery-option-id= "${deliveryOption.id}" >
                 <input type="radio" 
                 ${ischecked ? 'checked' : ''} class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
                 <div>
@@ -137,3 +139,9 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   })
 })
 
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const { productId, deliveryOptionId } = element.dataset
+    updateDeliveryOption(productId, deliveryOptionId)
+  })
+})
