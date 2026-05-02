095c566857f45ee96d7a9934ff0952843cf7b47c
degea <josekathata@gmail.com>
2026-05-02 19:38:45 +0300

    save data and generate html for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
-renderOrderSummary()
+console.log('checkout.js loaded');
+
+console.log('before renderOrderSummary');
+renderOrderSummary();
+console.log('after renderOrderSummary');
+
+console.log('before renderPaymentSummary');
+renderPaymentSummary();
+console.log('after renderPaymentSummary');
