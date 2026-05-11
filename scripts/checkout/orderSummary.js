25f4edaf80d1cc618b7ee6e608df18eed8589bf2
degea <josekathata@gmail.com>
2026-05-11 22:25:56 +0300

    create automated tests for ordersummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,7 +5,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
-import { deliveryOptions , getDeliveryOption} from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 
 /*
@@ -28,14 +28,14 @@ export function renderOrderSummary() {
 
     let matchingProduct = getProduct(productId)
 
-    
+
 
     // console.log(matchingProduct);
     const deliveryOptionId = cartItem.deliveryOptionId
 
     const deliveryOption = getDeliveryOption(deliveryOptionId)
 
-  
+
 
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days')
@@ -61,14 +61,15 @@ export function renderOrderSummary() {
               <div class="product-price">
                  $  ${formatCurrency(matchingProduct.priceCents)}
               </div>
-              <div class="product-quantity">
+              <div class="product-quantity 
+              js-product-quantity-${matchingProduct.id}">
                 <span>
                   Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                 </span>
                 <span class="update-quantity-link link-primary">
                   Update
                 </span>
-                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                   Delete
                 </span>
               </div>
