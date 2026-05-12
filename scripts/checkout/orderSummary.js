6882a63b33d1cf7c302da7778a553e3fe80ad0a4
degea <josekathata@gmail.com>
2026-05-12 11:31:28 +0300

    fixed a bug

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -45,6 +45,7 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += `
        <div class="cart-item-container 
+                  js-cart-item-container
                   js-cart-item-container-${matchingProduct.id}
                                                                ">
           <div class="delivery-date">
